a19c7ac22283b0bb25766c846cab3a0b063bd5c5
Aadithya <53986432+adivijay04@users.noreply.github.com>
2023-02-23 09:53:21 +0530

    Add files via upload

---
+++ build/exercise2/exercise2_generate_messages_cpp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/exercise2_generate_messages_cpp"
+  "/home/user/catkin_ws/devel/include/exercise2/num.h"
+  "/home/user/catkin_ws/devel/include/exercise2/SumOfInts.h"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/exercise2_generate_messages_cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
