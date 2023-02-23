3eeae37afb246c76c0dd84174717378fc4e1b3de
Aadithya <53986432+adivijay04@users.noreply.github.com>
2023-02-23 09:16:25 +0530

    Add files via upload

---
+++ src/exercise2/scripts/sum_of_integers_client.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+
+import sys
+import rospy
+from exercise2.srv import *
+
+
+def sum_of_ints_client(x):
+    rospy.wait_for_service('sum_of_ints')
+    try:
+        sum_of_ints = rospy.ServiceProxy('sum_of_ints', SumOfInts)
+        resp1 = sum_of_ints(x)
+        return resp1.sum
+    except rospy.ServiceException as e:
+        print("Service call failed: %s" % e)
+
+
+def usage():
+    return "%s [x]" % sys.argv[0]
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 3:
+        x = int(sys.argv[1])
+    else:
+        print(usage())
+        sys.exit(1)
+    print("Requesting for sum of %s integers" % (x))
+    print("The sum of %s is %s" % (x, sum_of_ints_client(x)))
